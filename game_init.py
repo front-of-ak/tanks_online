a5507f4e6f0f3cecce05a7808fdaf59b56ae6b82
front-of-ak <87611572+front-of-ak@users.noreply.github.com>
2021-12-26 22:06:18 +0300

    start screen done

--- game_init.py
+++ game_init.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+import pygame_widgets
+from pygame_widgets.button import Button
 import pygame
 
 pygame.init()
@@ -11,10 +12,18 @@ WIDTH = 1080
 HEIGHT = 800
 BACKGROUND = pygame.color.Color('black')
 
+GAME_TITLE = 'WWII: Величайшие танковые битвы'
+TITLE_SIZE = (458, 106)
+TITLE_COORDINATES = (50, 20)
+
+BUTTON_SIZE = 391, 62
+BUTTON_RADIUS = 10
+
+
 # screen and clock init
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-pygame.display.set_caption('WWII: Величайшие танковые битвы')
+pygame.display.set_caption(GAME_TITLE)
 # sprite groups
 all_sprites = pygame.sprite.Group()
 
@@ -26,7 +35,7 @@ def terminate():
 
 # functions, which are used for loading files
 def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('data', 'images', name)
     try:
         image = pygame.image.load(fullname).convert()
     except pygame.error as message:
@@ -42,6 +51,43 @@ def load_image(name, color_key=None):
     return image
 
 
+def break_start_screen():
+    global start_screen_run
+    start_screen_run = False
+
+
+start_screen_run = True
+
+
+# function for start screen
+def start_screen():
+    global start_screen_run
+    background = pygame.transform.scale(load_image('intro_screen.jpg'), (WIDTH, HEIGHT))
+    screen.blit(background, (0, 0))
+    logo = pygame.transform.scale(load_image('logo.png', -1), TITLE_SIZE)
+    screen.blit(logo, (WIDTH // 2 - logo.get_width() // 2, HEIGHT // 4 - logo.get_height()))
+    start_screen_run = True
+
+    button_play = Button(screen,
+                         WIDTH // 2 - BUTTON_SIZE[0] // 2, HEIGHT // 2, *BUTTON_SIZE,
+                         radius=BUTTON_RADIUS,
+                         image=load_image('play_btn.jpg'),
+                         onClick=break_start_screen
+                         )
+
+    while start_screen_run:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                terminate()
+            if not start_screen_run:
+                return
+        events = pygame.event.get()
+        pygame_widgets.update(events)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
 # main game cycle
 running = True
 while running:
